4face387255608af1c8ea137042670bb999f3022
jerry3dward <badiashfak42@gmail.com>
2023-10-11 17:34:51 +0530

    whole project completed in one commit so no history would be shown

--- chromeext/js.js
+++ chromeext/js.js
@@ -1,25 +1,51 @@
 let myleads = [];
 const inputEl = document.getElementById("input-el");
 const btn = document.querySelector("#input-btn");
-const ulEl = document.getElementById ("ul-el")
-
-btn.addEventListener ("click", function () {
-    myleads.push(inputEl.value);
-    renderlist()
-    inputEl.value = "";
-})
-function renderlist() {
-    let listitem = ""
-    for (let i =0; i < myleads.length; i++){
+const ulEl = document.getElementById ("ul-el");
+const delBtn = document.getElementById("del-btn");
+const tabBtn = document.getElementById("save-btn")
+const getleads = JSON.parse(localStorage.getItem("myleads"));
+if (getleads) {
+    myleads = getleads;
+    renderlist(myleads)
+}
+function renderlist(leads) {
+    let listitem = "";
+    for (let i =0; i < leads.length; i++){
         // listitem += "<li> <a target ='_blank' href='" + myleads[i] +"'>" + myleads[i] +"</a></li>"
+        // used templates litterals instead
         listitem += `
         <li> 
-        <a target='_blank' href='${myleads[i]}'>
-            ${myleads[i]}
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
         </a>
         <li>
         `
-        console.log(listitem)
     }
     ulEl.innerHTML = listitem
 }
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, 
+    function (tabs) {
+        myleads.push(tabs[0].url)
+        localStorage.setItem("myleads", JSON.stringify(myleads));
+        renderlist(myleads)
+    })
+    // console.log(tabs[0].url);
+})
+delBtn.addEventListener("click", function(){
+    localStorage.clear();
+    myleads = [];
+    renderlist(myleads);
+})
+btn.addEventListener ("click", function () {
+    myleads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myleads", JSON.stringify(myleads));
+    renderlist(myleads)
+})
+
+
